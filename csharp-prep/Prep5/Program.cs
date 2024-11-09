61d098704d2f604bff195e9b9088e26000506a07
Daniel Hansen <han23075@byui.edu>
2024-11-09 12:10:00 -0700

    - Program 5 aims use the users input and make a square from it. - - The program uses multiple functions to answer a persons input The program starts with asking the persons name and their favorite number/ from these inputs the system gives out a print statement that has the squared value of the user as well as their name.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,33 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName(){
+        Console.Write("Enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber(){
+        Console.Write("Enter favorite number: ");
+        return int.Parse(Console.ReadLine());
+    } 
+    static double SquareNumber(int fav_number){
+        double squared = Math.Pow(fav_number, 2);
+        return squared;
+    } 
+    static void DisplayResult(string name, double squared){
+        Console.WriteLine($"{name}, the square of your number is {squared}. ");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int fav_number = PromptUserNumber();
+        double squared = SquareNumber(fav_number);
+
+        DisplayResult(name, squared);
+
     }
 }
