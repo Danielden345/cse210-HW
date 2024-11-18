92ae97d99db7a9a0bd6eed7813ff9649c4f37b6d
Daniel Hansen <han23075@byui.edu>
2024-11-17 23:16:03 -0700

    - This program puts together two files. - - Using jobs and resume, I put together two specific files that speificied two different functions. These files combine to give the information about a person's job history with their name.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
 
+
+
+
 class Program
 {
-    static void Main(string[] args)
+    static void DisplayJobDetails(){
+
+    }
+    static void Main()
     {
         Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Electrician";
+        job1._company = "Delta Electric";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+        Job job2 = new Job();
+        job2._jobTitle = "Sandwich Artist";
+        job2._company = "Subway";
+        job2._startYear = 2020;
+        job2._endYear = 2021;
+        Resume yourResume  = new Resume();
+        yourResume._name = "John Stanton";
+        yourResume._jobs.Add(job1);
+        yourResume._jobs.Add(job2);
+        yourResume.Display();
+
     }
 }
